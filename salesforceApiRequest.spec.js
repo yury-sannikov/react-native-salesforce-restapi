9b7b5e866f7b12889ae34e409ed608ea18fdc99e
Yuriy Sannikov <ysannikov@fonteva.com>
2017-01-18 14:54:34 -0500

    wire up es6

--- salesforceApiRequest.spec.js
+++ salesforceApiRequest.spec.js
@@ -1,4 +1,10 @@
 
+jest.mock('react-native', () => {
+
+});
+
+import { SalesforceApiRequest } from './salesforceApiRequest'
+
 describe('Salesforce API', () => {
 	it('should work', () => {
 		expect(true).toBeTruthy();
