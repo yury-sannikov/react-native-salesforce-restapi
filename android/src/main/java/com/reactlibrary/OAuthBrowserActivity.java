38ecac22ae02f09df98785917f65bf49c45fc4e3
Yuriy Sannikov <ysannikov@fonteva.com>
2017-01-25 12:00:52 -0500

    Implement login webview with oauth callback URL handling, logout with cookies clear logic

--- android/src/main/java/com/reactlibrary/OAuthBrowserActivity.java
+++ android/src/main/java/com/reactlibrary/OAuthBrowserActivity.java
@@ -1,34 +1,54 @@
 package com.reactlibrary;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class OAuthBrowserActivity extends AppCompatActivity {
+
+    private WebView webView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oauth_browser);
 
-        Button  button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Intent intent = getIntent();
+        Uri loginUri = intent.getData();
+        final String callbackUri = intent.getStringExtra(RNSalesforceRestapiModule.ACTIVITY_EXTRA_CALLBACK_URI);
+
+        webView = (WebView) findViewById(R.id.loginWebView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
             @Override
-            public void onClick(View v) {
-                setResult(554);
-                finish();
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.contains(callbackUri)) {
+                    Intent resultIntent = new Intent();
+                    resultIntent.setData(Uri.parse(url));
+                    setResult(RNSalesforceRestapiModule.ACTIVITY_RESPONSE_CODE, resultIntent);
+                    finish();
+                    return false;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
             }
         });
+        webView.loadUrl(loginUri.toString());
+    }
 
-        Button button2 = (Button)findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setResult(202);
-                finish();
-            }
-        });
-
+    @Override
+    public void onBackPressed() {
+        setResult(RNSalesforceRestapiModule.ACTIVITY_RESPONSE_CODE_CANCEL);
+        finish();
     }
 }
--- android/src/main/java/com/reactlibrary/RNSalesforceRestapiModule.java
+++ android/src/main/java/com/reactlibrary/RNSalesforceRestapiModule.java
@@ -3,8 +3,13 @@ package com.reactlibrary;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Debug;
 import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.ValueCallback;
 
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.BaseActivityEventListener;
@@ -18,6 +23,12 @@ import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.ReadableType;
 
 public class RNSalesforceRestapiModule extends ReactContextBaseJavaModule implements ActivityEventListener{
+
+  public static final String ACTIVITY_EXTRA_CALLBACK_URI = "extra_callback_uri";
+  public static final int ACTIVITY_RESPONSE_CODE = 101;
+  public static final int ACTIVITY_RESPONSE_CODE_CANCEL = 102;
+
+  private static final int ACTIVITY_REQUEST_CODE = 100;
 
   private final ReactApplicationContext reactContext;
   private Promise tokenPromise = null;
@@ -34,18 +45,34 @@ public class RNSalesforceRestapiModule extends ReactContextBaseJavaModule implem
   }
 
   @ReactMethod
-  public void loginUser(ReadableMap parameters, Promise promise) {
-    ReadableMapKeySetIterator iterator = parameters.keySetIterator();
-    while (iterator.hasNextKey()) {
-      String key = iterator.nextKey();
-      ReadableType type = parameters.getType(key);
-
-      Log.d("Key", key);
-      Log.d("Type", type.toString());
-    }
+  public void loginUser(String uri, String callbackUri, Promise promise) {
     tokenPromise = promise;
     Intent intent = new Intent(this.reactContext, OAuthBrowserActivity.class);
-    this.reactContext.startActivityForResult(intent, 554, null);
+    intent.setData(Uri.parse(uri));
+    intent.putExtra(ACTIVITY_EXTRA_CALLBACK_URI, callbackUri);
+    this.reactContext.startActivityForResult(intent, ACTIVITY_REQUEST_CODE, null);
+  }
+  @ReactMethod
+  public void logout(final Promise promise) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      CookieManager.getInstance().removeAllCookies(new ValueCallback<Boolean>() {
+        @Override
+        public void onReceiveValue(Boolean value) {
+          if (promise != null) {
+            promise.resolve(value);
+          }
+        }
+      });
+    } else {
+      CookieSyncManager cookieSyncMngr=CookieSyncManager.createInstance(this.reactContext);
+      cookieSyncMngr.startSync();
+      CookieManager cookieManager=CookieManager.getInstance();
+      cookieManager.removeAllCookie();
+      cookieManager.removeSessionCookie();
+      cookieSyncMngr.stopSync();
+      cookieSyncMngr.sync();
+      promise.resolve(true);
+    }
   }
 
   @Override
@@ -55,17 +82,19 @@ public class RNSalesforceRestapiModule extends ReactContextBaseJavaModule implem
   @Override
   public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
 
-    if (tokenPromise != null) {
-      if (resultCode == 554) {
-        tokenPromise.resolve(new String("Resolved"));
-      } else {
-        tokenPromise.reject(String.valueOf(resultCode), "Rejected");
-      }
-      tokenPromise = null;
+    if (requestCode != ACTIVITY_REQUEST_CODE) {
+      Log.d("OAuth onActivityResult", "Unknown request code " + String.valueOf(requestCode));
+      return;
     }
-    Log.d("requestCode", String.valueOf(requestCode));
-    Log.d("resultCode", String.valueOf(resultCode));
-    //Log.d("data", data.toString());
+    if (tokenPromise == null) {
+      return;
+    }
 
+    if (resultCode != ACTIVITY_RESPONSE_CODE) {
+      tokenPromise.reject(String.valueOf(resultCode), "Rejected");
+    } else {
+      tokenPromise.resolve(data.getData().toString());
+    }
+    tokenPromise = null;
   }
 }
